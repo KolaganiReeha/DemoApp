b95da6fb9c72faffdcab03051bd0fdbe268054a9
reehakolagan <reehakolagani1@gmail.com>
2024-01-20 17:48:16 -0600

    First Commit

---
+++ DemoApp/src/DemoApp.java
@@ -0,0 +1,14 @@
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		char[] arr = {'R','a','m'};
+		System.out.println(String.valueOf(arr));
+		
+		
+	}
+
+}
